e8d028a584b83bede2a3aadfd832b71ac0fbe1e0
Claus Kovacs <claus@kovacs.cf>
2021-07-21 12:25:45 +0100

    +interests

--- index.php
+++ index.php
@@ -1,21 +1,94 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
 		<title></title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="CSS/style.css">
+		<meta charset = "utf-8">
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css">
 	</head>
+
 	<body>
-		<b>Where to find me:</b>
-		<br>
-		Youtube: <a class="customlink" href ="https://www.youtube.com/V9ZIE" target="_blank">V9ZIE</a>
-		<br>
-		Github: <a class="customlink" href ="https://github.com/clauskovacs" target="_blank">clauskovacs</a>
-		<br>
+		<div class = "emphText">Where to find me:</div>
+		<ul class = "mainpageListFirst">
+			<li>
+				Youtube: <a class = "customlink" href = "https://www.youtube.com/V9ZIE" target = "_blank">V9ZIE</a>
+			</li>
+
+			<li>
+				Github: <a class = "customlink" href = "https://github.com/clauskovacs" target = "_blank">clauskovacs</a>
+			</li>
+		</ul>
+
 		<br>
 
-		<b>Contact:</b>
+		<div class = "emphText">Interests:</div>
+		<ul class = "mainpageListFirst">
+			<li style = "padding-top: 0.25em;"><b>Physics</b>
+				<ul class = "mainpageListSecond">
+					<li>
+						<a	class = "customlink"
+							href = "physics/simulation_of_electron_backscatter_using_PENELOPE.pdf"
+							target = "_blank">
+							Bachelor's Thesis</a>
+							(Simulation of Electron Backscattering using PENELOPE)
+					</li>
+
+					<li>
+						<a	class = "customlink"
+							href = "physics/measurement_heat_capactiy_at_low_temperatures.pdf"
+							target = "_blank">
+							Project Thesis 1</a>
+							(Measurement of the Specific Heat Capacity at Low Temperatures)
+					</li>
+
+					<li>
+						<a	class = "customlink"
+							href = "physics/electrochemical_cell_for_single_crystal_samples.pdf"
+							target = "_blank">
+							Project Thesis 2</a>
+							(Electrochemical Cell for Single Crystal Samples)
+					</li>
+
+					<li>
+						<a	class = "customlink"
+							href = "physics/numerical_simulation_electrochemical_flowcell.pdf"
+							target = "_blank">
+							Master's Thesis</a>
+							(Numerical Simulation of an Electrochemical Flow Cell using Finite-Element Simulations)
+					</li>
+				</ul>
+
+			<li style = "padding-top: 0.25em;"><b>Programming, Webdesign</b>
+				<ul class = "mainpageListSecond">
+					<li>
+						<a	class = "customlink"
+							href = "http://h.ik.cx/"
+							target = "_blank">
+							h.ik.cx</a>
+							(file hosting for university exams of the TU Wien)
+					</li>
+
+					<li>
+						<a	class = "customlink"
+							href = "http://phys.ik.cx/"
+							target = "_blank">
+							phys.ik.cx</a>
+							(personal site)
+					</li>
+				</ul>
+
+			</li>
+		</ul>
+
+
 		<br>
-		claus[at]kovacs.cf
+
+		<div class = "emphText">Contact:</div>
+		
+		<ul class = "mainpageListFirst">
+			<li>claus[at]kovacs.cf</li>
+		</ul>
+
 	</body>
+
 </html>
